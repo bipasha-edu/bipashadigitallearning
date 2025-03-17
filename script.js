a43bc62f02557be41c11e2ff3374676c41255c0c
Bipasha Digital Learning <12bipasha2004@gmail.com>
2025-03-17 09:50:40 +0530

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function togglemenu(){
-  var sidebar=document.getElementById("sidebar");
+  var sidebar=document.getElementById("closeMenu").addEventListener("click", function(){
+  document.querySelector(".sidebar").style.display="none";
+  });
+
   if(sidebar.style.display==="block"){
     sidebar.style.display="none";
   } else {
